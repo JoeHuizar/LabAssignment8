eafa07f492153448a2f4ee8cfb4b8a54adf1898a
JoeHuizar <joehuizar75@gmail.com>
2018-11-15 19:05:59 -0800

    finished Lab Assignment 8

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from './contact.model';
 import { Http } from '@angular/http';
+import { LocalStorageService } from '../localStorageService';
+import { ActivatedRoute } from '@angular/router';
+import { IUser } from '../login/login.component';
+import { Router } from '@angular/router';
+import { ToastService } from '../toast/toast.service';
 
 @Component({
   selector: 'contact',
@@ -11,10 +16,26 @@ export class ContactComponent implements OnInit {
 
   contacts: Array<Contact> = [];
   contactParams: string = '';
-  constructor(private http: Http) { }
+  localStorageService: LocalStorageService<Contact>;
+  currentUser: IUser;
+  constructor(
+    private http: Http,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastService) {
+    this.localStorageService = new LocalStorageService('contacts');
+  }
 
   async ngOnInit() {
+    const currentUser = this.localStorageService.getItemsFromLocalStorage('user');
+    if (currentUser === null) {
+      this.router.navigate(['login']);
+    }
     this.loadContacts();
+    this.activatedRoute.params.subscribe((data: IUser) => {
+      console.log('data passed from login component to this component: ', data);
+      this.currentUser = data;
+    });
   }
 
   async loadContacts() {
@@ -41,26 +62,36 @@ export class ContactComponent implements OnInit {
     this.saveItemsToLocalStorage(this.contacts);
   }
 
-  saveContact(contact: Contact) {
-    contact.editing = false;
-    this.saveItemsToLocalStorage(this.contacts);
+  saveContact(contact: any) {
+
+    const id = contact['id'];
+    const firstName = contact.firstName;
+    
+    if (id == null || firstName == null) {
+      this.toastService.showToast('danger', 2000, 'Saved failed!');
+    } else {
+      contact.editing = false;
+      this.saveItemsToLocalStorage(this.contacts);
+    }
   }
 
   saveItemsToLocalStorage(contacts: Array<Contact>) {
     contacts = this.sortByID(contacts);
-    const savedContacts = localStorage.setItem('contacts', JSON.stringify(contacts));
-    return savedContacts;
+    return this.localStorageService.saveItemsToLocalStorage(contacts);
+    // const savedContacts = localStorage.setItem('contacts', JSON.stringify(contacts));
+    // return savedContacts;
   }
 
   getItemsFromLocalStorage(key: string) {
-    const savedContacts = JSON.parse(localStorage.getItem(key));
-    return savedContacts;
+    // const savedContacts = JSON.parse(localStorage.getItem(key));
+    return this.localStorageService.getItemsFromLocalStorage();
+    // return savedContacts;
   }
 
   searchContact(params: string) {
     this.contacts = this.contacts.filter((item: Contact) => {
       const fullName = item.firstName + ' ' + item.lastName;
-      if(params === fullName || params === item.firstName || params === item.lastName) {
+      if (params === fullName || params === item.firstName || params === item.lastName) {
         return true;
       } else {
         return false;
@@ -70,9 +101,14 @@ export class ContactComponent implements OnInit {
 
   sortByID(contacts: Array<Contact>) {
     contacts.sort((prevContact: Contact, presContact: Contact) => {
-      return prevContact.id > presContact.id ? 1: -1;
+      return prevContact.id > presContact.id ? 1 : -1;
     });
     return contacts;
+  }
+
+  logout() {
+    this.localStorageService.clearItemFromLocalStorage('user');
+    this.router.navigate(['']);
   }
 
 }
